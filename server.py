41668ec135077bf65546ca4102188fee9d60f8e1
Jude Bogdan Laurentiu <43954492+kogaion28@users.noreply.github.com>
2020-01-15 16:10:19 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -2,27 +2,27 @@
 #Librari
 
 import socket
-import argparse
+import argparse #Pentru linile de comanda si subcomenzi
 import threading 
 
-parser = argparse.ArgumentParser(description = "This is the server for the multithreaded socket demo!")
+parser = argparse.ArgumentParser(description = "Acesta este servarul!")
 parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = socket.gethostname())
 parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 9999)
 args = parser.parse_args()
 
 print(f"Ruleaza servarul pe hostul: {args.host} si portul: {args.port}")
 
-sck = socket.socket()
-sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sck = socket.socket()# creiem socket
+sck.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #vrem sa setam optiunea  sa lase un socket server in rezerva dupa ce servarul a fost abandonat
 
 try: 
 	sck.bind((args.host, args.port))
-	sck.listen(5)
-except Exception as e:
+	sck.listen(5) #
+except Exception as e: # exceptie iesire daca servarul/hostul esueaza
 	raise SystemExit(f"Nu am putut face conexiunea dintre server si hostul: {args.host} la  portul: {args.port}, din cauza: {e}")
 
 
-def on_new_client(client, connection):
+def on_new_client(client, connection):#argumete cliente si connection pentru client
 	ip = connection[0]
 	port = connection[1]
 	print(f"Noua conexiune a fost facuta de la  {ip}, si portul: {port}!")
